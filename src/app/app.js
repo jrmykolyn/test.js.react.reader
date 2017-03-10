51f7f93b2b5a24a1bf80e623d2cd58ac621b6ea6
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-10 17:51:32 -0500

    Completed first pass of 'App' component.

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,22 @@
-( () => {
-  console.log( 'INSIDE APP `IIFE`' );
-} )();
+// --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+import { React } from 'react';
+
+
+// --------------------------------------------------
+// DEFINE COMPONENT
+// --------------------------------------------------
+export default class App extends React.Component {
+    constructor() {
+        console.log( 'INSIDE `App` CONSTRUCTOR.' ); /// TEMP
+        super();
+    } // /constructor()
+
+
+    render() {
+        console.log( 'INSIDE `App` `render()`.' ); /// TEMP
+
+        return (<h1>App</h1>);
+    } // /render()
+} // /App
