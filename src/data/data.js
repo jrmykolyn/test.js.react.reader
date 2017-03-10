ebc4da5fe04c85ad02429b0b0ca3aa6c5787b740
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-10 18:11:33 -0500

    Started adding mock/dummy data to project.

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const DATA = [
+    {
+        "title": "Title 1",
+        "timestamp": 1489186405946
+    },
+    {
+        "title": "Title 2",
+        "timestamp": 1489186407074
+    },
+    {
+        "title": "Title 3",
+        "timestamp": 1489186407481
+    }
+]
